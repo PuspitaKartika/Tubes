129959b607545eee8f6f3823ab03650a427cd61e
PuspitaKartika <belajarpuspita4@gmail.com>
2023-07-07 02:42:26 +0700

    validate login

--- Tubes/src/backend/Login.java
+++ Tubes/src/backend/Login.java
@@ -12,7 +12,6 @@ public class Login {
         try {
             Database.openDb();
             String sql = "SELECT name, pass FROM users Where id_role LIKE 1";
-
             ResultSet rs = Database.s.executeQuery(sql);
 
             while (rs.next()){
@@ -22,7 +21,33 @@ public class Login {
                 if(dbUserName.equals(name) && dbPassword.equals(pass)){
                     Database.closeDb();
                     return  true;
+                }
+            }
+            Database.closeDb();
+            return false;
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean validatePengguna(String name, String pass){
+        String dbUserName, dbPassword;
+
+        try {
+            Database.openDb();
+            String sql = "SELECT name, pass FROM users Where id_role LIKE 2";
+            ResultSet rs = Database.s.executeQuery(sql);
+
+            while (rs.next()){
+                dbUserName = rs.getString("name");
+                dbPassword = rs.getString("pass");
+
+                if(dbUserName.equals(name) && dbPassword.equals(pass)){
+                    Database.closeDb();
+                    return  true;
                 }
             }
             Database.closeDb();
